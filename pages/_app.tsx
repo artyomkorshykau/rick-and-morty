b3a448b487881a1bfc2d0a0b69a638e6ccff361e
atryomkorshykau <butovichjunior@gmail.com>
2024-03-26 01:31:27 +0300

    app init

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,26 @@
 import "@/styles/globals.css";
-import type { AppProps } from "next/app";
+import type {AppProps} from "next/app";
+import {NextPage} from "next";
+import {ReactElement, ReactNode, useState} from "react";
+import {QueryClient, QueryClientProvider} from '@tanstack/react-query'
 
-export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />;
+
+export type NextPageWithLayout<P = {}, IP = P> = NextPage<P, IP> & {
+  getLayout?: (page: ReactElement) => ReactNode
+}
+
+type AppPropsWithLayout = AppProps & {
+  Component: NextPageWithLayout
+}
+
+export default function App({Component, pageProps}: AppPropsWithLayout) {
+
+  const [queryClient] = useState(() => new QueryClient)
+
+  const getLayout = Component.getLayout ?? ((page) => page)
+  return getLayout(
+      <QueryClientProvider client={queryClient}>
+          <Component {...pageProps}/>
+      </QueryClientProvider>
+  )
 }
