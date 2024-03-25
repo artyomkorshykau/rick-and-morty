96231126543c117d6a9b315f44bc56dda9fb7ff3
atryomkorshykau <butovichjunior@gmail.com>
2024-03-26 01:36:33 +0300

    feat: layout component

---
+++ components/Layout/layout.tsx
@@ -0,0 +1,16 @@
+import {NextPage} from "next";
+import {PropsWithChildren, ReactElement} from "react";
+import Navbar from "@/components/Navbar/navbar";
+
+export const Layout: NextPage<PropsWithChildren> = ({children}) => {
+  return (
+      <main>
+        <Navbar/>
+        {children}
+      </main>
+  )
+}
+
+export const getLayout = (page: ReactElement) => {
+  return <Layout>{page}</Layout>
+}
