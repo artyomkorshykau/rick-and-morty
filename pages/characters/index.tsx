1aed2d894872afa8b51af2cb9dc6c1ce0ae10ebd
atryomkorshykau <butovichjunior@gmail.com>
2024-03-28 21:18:22 +0300

    edit: characters page wrapper

--- pages/characters/index.tsx
+++ pages/characters/index.tsx
@@ -1,15 +1,16 @@
-import {CharacterCard} from "@/components/Character/characterCard";
+import {CharacterCard} from "@/components/CharacterCard/characterCard";
 import HeadMeta from "@/components/HeadMeta/head";
-import {getLayout} from "@/components/Layout/layout";
 import Link from "next/link";
 import s from './characters.module.scss'
 import axios from "axios";
 import {CharacterType} from "@/assets/types/character";
 import {ResponseType} from "@/assets/types/response";
+import {getLayout} from "@/components/Layout/Base/base-layout";
+import {PageWrapper} from "@/components/PageWrapper/page-wrapper";
 
 
 export const getStaticProps = async () => {
-  const characters = await axios.get<ResponseType<CharacterType>>('https://rickandmortyapi.com/api/character')
+  const characters = await axios.get<ResponseType<CharacterType>>(`${process.env.NEXT_PUBLIC_RAM_API}/character`)
       .then(res => res.data.results)
 
   if (!characters) {
@@ -32,15 +33,16 @@ type Props = {
 function Characters({characters}: Props) {
 
   return (
-      <div className={s.charactersPage}>
-        <HeadMeta title={'Characters | Rick & Morty'}/>
-        {characters && characters.map(el => (
-            <Link href={`/characters/${el.id}`} key={el.id}>
-              <CharacterCard character={el}
-              />
-            </Link>
-        ))}
-      </div>
+      <PageWrapper title={'Characters | Rick & Morty'}>
+        <div className={s.charactersPage}>
+          {characters && characters.map(el => (
+              <Link href={`/characters/${el.id}`} key={el.id}>
+                <CharacterCard character={el}
+                />
+              </Link>
+          ))}
+        </div>
+      </PageWrapper>
   );
 }
 
