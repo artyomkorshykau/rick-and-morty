372d5a0798750531ec1c6f1986a5bc0bface2d09
atryomkorshykau <butovichjunior@gmail.com>
2024-03-29 18:49:17 +0300

    feat: dynamic pagination

---
+++ pages/characters/hook/usePagination.ts
@@ -0,0 +1,44 @@
+import {useEffect, useState} from "react";
+import {CharacterType} from "@/assets/types/character";
+import axios from "axios";
+import {ResponseType} from "@/assets/types/response";
+
+
+export const usePagination = () => {
+  const [page, setPage] = useState<number>(0)
+  const [items, setItems] = useState<CharacterType[]>([])
+  const [totalItems, setTotalItems] = useState<number>(0)
+  const [fetching, setFetching] = useState<boolean>(true)
+
+  useEffect(() => {
+    if (fetching) {
+      axios.get<ResponseType<CharacterType>>(`${process.env.NEXT_PUBLIC_RAM_API}/character/?page=${page}`)
+          .then(res => {
+                setItems([...items, ...res.data.results])
+                setPage(prevState => prevState + 1)
+                setTotalItems(res.data.info.count)
+              }
+          )
+          .finally(() => {
+            setFetching(false)
+          })
+    }
+
+  }, [fetching, items, page])
+
+  useEffect(() => {
+    window.document.addEventListener('scroll', scrollHandler)
+
+    return () => {
+      window.document.removeEventListener('scroll', scrollHandler)
+    }
+  })
+
+  const scrollHandler = (e: any) => {
+    if (e.currentTarget.documentElement.scrollHeight - (e.currentTarget.documentElement.scrollTop + window.innerHeight) < 100 && items.length < totalItems) {
+      setFetching(true)
+    }
+  }
+
+  return items
+}
--- pages/characters/index.tsx
+++ pages/characters/index.tsx
@@ -1,41 +1,39 @@
 import {CharacterCard} from "@/components/CharacterCard/characterCard";
-import HeadMeta from "@/components/HeadMeta/head";
 import Link from "next/link";
 import s from './characters.module.scss'
-import axios from "axios";
-import {CharacterType} from "@/assets/types/character";
-import {ResponseType} from "@/assets/types/response";
 import {getLayout} from "@/components/Layout/Base/base-layout";
 import {PageWrapper} from "@/components/PageWrapper/page-wrapper";
+import {usePagination} from "@/pages/characters/hook/usePagination";
 
+// export const getStaticProps = async () => {
+//   const characters = await axios.get<ResponseType<CharacterType>>(`${process.env.NEXT_PUBLIC_RAM_API}/character`)
+//       .then(res => res.data.results)
+//
+//   if (!characters) {
+//     return {
+//       notFound: true
+//     }
+//   }
+//
+//   return {
+//     props: {
+//       characters
+//     }
+//   }
+// }
+//
+// type Props = {
+//   characters: CharacterType[]
+// }
 
-export const getStaticProps = async () => {
-  const characters = await axios.get<ResponseType<CharacterType>>(`${process.env.NEXT_PUBLIC_RAM_API}/character`)
-      .then(res => res.data.results)
+function Characters() {
 
-  if (!characters) {
-    return {
-      notFound: true
-    }
-  }
-
-  return {
-    props: {
-      characters
-    }
-  }
-}
-
-type Props = {
-  characters: CharacterType[]
-}
-
-function Characters({characters}: Props) {
+  const items = usePagination()
 
   return (
       <PageWrapper title={'Characters | Rick & Morty'}>
         <div className={s.charactersPage}>
-          {characters && characters.map(el => (
+          {items && items.map(el => (
               <Link href={`/characters/${el.id}`} key={el.id}>
                 <CharacterCard character={el}
                 />
