9a057cd3667c23fb7d8b4ddbdbdb2f12e390660f
atryomkorshykau <butovichjunior@gmail.com>
2024-04-01 02:41:04 +0300

    feat: dynamic pagination

--- assets/api/api.ts
+++
@@ -1,8 +0,0 @@
-import { instance } from "./instances";
-import { RickAndMortyApi } from "./rick-and-morty-api";
-
-const rickAndMortyApi = new RickAndMortyApi(instance);
-
-export const API = {
-  rickAndMorty: rickAndMortyApi,
-};
